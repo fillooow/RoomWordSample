5b2d3edc3f4cb649a4f7af3bc2e411a0199a22b4
Filipp Shadrin <shadrin_ph@tochka.com>
2020-05-02 21:51:24 +0500

    Добавил DAO и entity для будущей работы с бд

---
+++ app/src/main/java/fillooow/app/roomwordsample/db/dao/WordDAO.kt
@@ -0,0 +1,29 @@
+package fillooow.app.roomwordsample.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import fillooow.app.roomwordsample.db.entity.Word
+
+/**
+ * DAO (data access object) - в дао задаются запросы,
+ * которые в нем же связываются с методами их вызова.
+ *
+ * DAO должен быть интерфейсом или абстрактным классом.
+ *
+ * Запросы должны вызваться не на UI потоке.
+ */
+@Dao
+interface WordDAO {
+
+    // Получаем все слова в алфавитном порядке
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE) // Если такая запись уже есть в таблице, ничего не делаем
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table") // Для удаления нескольких сущностей нет отдельной аннотации, так что используем @Query
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/fillooow/app/roomwordsample/db/entity/Word.kt
@@ -0,0 +1,16 @@
+package fillooow.app.roomwordsample.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Entity - представляют собой SQLite таблицу
+ */
+@Entity(tableName = "word_table") // Представляет собой SQLite таблицу с именем word_table
+data class Word(
+
+    @PrimaryKey
+    @ColumnInfo(name = "word") // по умолчанию имя колонки будет совпадать с именем переменой. Тут для примера так же назвали.
+    val word: String // Переменные для БД должны быть публичными
+)

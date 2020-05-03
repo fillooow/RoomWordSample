18e04e623dac2a1af53cf1bedf411debfa70cb5f
Filipp Shadrin <shadrin_ph@tochka.com>
2020-05-04 01:38:23 +0500

    Переместил активити

--- app/src/main/java/fillooow/app/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/fillooow/app/roomwordsample/NewWordActivity.kt
@@ -1,11 +1,10 @@
-package fillooow.app.roomwordsample.db
+package fillooow.app.roomwordsample
 
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import fillooow.app.roomwordsample.R
 import kotlinx.android.synthetic.main.activity_new_word.*
 
 class NewWordActivity : AppCompatActivity() {

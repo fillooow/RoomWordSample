feb7d91a3e3304e2c9be7ecb00fb345e87489765
Filipp Shadrin <shadrin_ph@tochka.com>
2020-05-04 01:08:40 +0500

    Добавил адаптер для RecylcerView

---
+++ app/src/main/java/fillooow/app/roomwordsample/ui/adapter/WordListAdapter.kt
@@ -0,0 +1,39 @@
+package fillooow.app.roomwordsample.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import fillooow.app.roomwordsample.R
+import fillooow.app.roomwordsample.db.entity.WordEntity
+
+class WordListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var words = emptyList<WordEntity>() // Кеш для слов
+
+    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = words[position]
+        holder.wordItemView.text = current.word
+    }
+
+    internal fun setWords(words: List<WordEntity>) {
+        this.words = words
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = words.size
+}
